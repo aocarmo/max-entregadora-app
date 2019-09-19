687ec768065d1778cf910165f017329758004b11
PGMS\aocarmo <alex.carmo91@gmail.com>
2019-09-19 15:00:40 -0300

    Ajustes na telas solicitado por Rander

--- src/pages/minhas-entregas-list/minhas-entregas-list.ts
+++ src/pages/minhas-entregas-list/minhas-entregas-list.ts
@@ -3,6 +3,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { SuperTabsController } from 'ionic2-super-tabs';
 
 import {ENTREGAS} from "../../mocks/mock-entregas"; // So para exemplos
+import _ from 'lodash';
 
 /**
  * Generated class for the MinhasEntregasListPage page.
@@ -24,13 +25,35 @@ export class MinhasEntregasListPage {
 
   public entregas: any;
   rootNavCtrl: NavController;
+  queryText: string;
+  todasEntregas: any;
+
+
  
   constructor(public navCtrl: NavController, public navParams: NavParams, private superTabsCtrl: SuperTabsController) {
     this.rootNavCtrl = this.navParams.get('rootNavCtrl');
+    this.queryText = "";
     this.entregas = ENTREGAS;
+    this.todasEntregas = this.entregas;
+    
   }
 
 
+  filterCidade(cid: any){
+    let val = cid.target.value;
+
+    if(val  && val.trim() != ''){
+      this.entregas = _.values(this.todasEntregas);
+      this.entregas = this.entregas.filter((entrega)=>{
+        return (
+          entrega.devedor.toLowerCase().indexOf(val.toLowerCase()) > -1 ||
+          entrega.documento.toLowerCase().indexOf(val.toLowerCase()) > -1 )
+      });
+    }else{
+      this.entregas = this.todasEntregas;
+    }
+
+  }
   verDetalhe(entrega:any){
     this.rootNavCtrl.push('page-detalhe-entrega',{'entrega': entrega }); 
   }
